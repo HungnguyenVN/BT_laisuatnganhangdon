06e97cee158c09b5cde2b88835196a9851ef9ed3
HungnguyenVN <hungnguyen.cnn@gmail.com>
2021-06-04 10:05:39 +0700

    laisuatnganhang

--- my.js
+++ my.js
@@ -1,5 +1,5 @@
 let a = +prompt('nhap so tien vay');
-let b = +prompt('nhap lai suat ');
+let b = +prompt('nhap lai suat cua 1 nam ');
 let n = +prompt('nhap so nam ');
 tientra = a + a*b/100*n;
 document.write('số tiền phải trả cả gốc lẫn lãi là '+ tientra);
